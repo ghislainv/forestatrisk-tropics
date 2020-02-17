444a0136198a772983622a6386c6a97a6eb32281
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2020-02-17 12:18:01 +0100

    Add Africa-mbb.py

---
+++ Africa/Africa-mbb.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# ==============================================================================
+# author          :Ghislain Vieilledent
+# email           :ghislain.vieilledent@cirad.fr, ghislainv@gmail.com
+# web             :https://ghislainv.github.io
+# python_version  :2.7
+# license         :GPLv3
+# ==============================================================================
+
+# PRIOR TO EXECUTING THE FOLLOWING SCRIPT, AUTHENTICATE TO
+# 1. Google Earth Engine: earthengine authenticate
+# 2. Google Drive with rclone: https://rclone.org/drive/
+
+import sys
+import os
+import forestatrisk as far
+from run_modelling_steps import run_modelling_steps
+
+
+#index_ctry = int(sys.argv[1])
+index_ctry = 24 # REU
+
+# ==================
+# Settings
+# PROJ_LIB
+os.environ["PROJ_LIB"] = "/home/ghislain/miniconda3/envs/conda-far/share/proj"
+# Earth engine
+import ee
+ee.Initialize()
+# WDPA API
+from dotenv import load_dotenv
+load_dotenv("/home/ghislain/Code/forestatrisk-tropics/.env")
+from pywdpa import get_token
+get_token()
+# ==================
+
+# Print iso3
+iso3 = ['AGO', 'BDI', 'BEN', 'CAF', 'CIV', 'CMR', 'COD', 'COG', 'COM',
+        'ETH', 'GAB', 'GHA', 'GIN', 'GMB', 'GNB', 'GNQ', 'KEN', 'LBR',
+        'MDG', 'MOZ', 'MUS', 'MWI', 'MYT', 'NGA', 'REU', 'RWA', 'SEN',
+        'SLE', 'SSD', 'STP', 'TGO', 'TZA', 'UGA', 'ZMB']
+
+# Original working directory
+owd = os.getcwd()
+
+# Function for multiprocessing
+def run_country(iso3):
+    
+    # Make new directory for country
+    os.chdir(owd)
+    far.make_dir(iso3)
+    os.chdir(os.path.join(owd, iso3))
+    
+    # Data
+    far.data.country(iso3=iso3,
+                     proj="EPSG:3395",
+                     data_country=True,
+                     data_forest=True,
+                     keep_data_raw=True,
+                     fcc_source="jrc",
+                     gdrive_remote_rclone="gdrive_gv",
+                     gdrive_folder="GEE-forestatrisk-tropics")
+    
+    # Model and Forecast
+    run_modelling_steps(fcc_source="jrc")
+    
+    # Return country iso code
+    return(iso3)
+
+
+# Run country
+run_country(iso3[index_ctry])
+
+# End
