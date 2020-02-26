b96c3e706f8aa6ee2f4dcfa97359ef993577622c
gvieilledent <gvieilledent@mbbnode-0-21.local>
2020-02-26 17:13:00 +0100

    Add Asia-mbb.py

--- Asia/Asia-mbb.py
+++ Asia/Asia-mbb.py
@@ -36,20 +36,18 @@ get_token()
 # ==================
 
 # Set working directory to nas
-owd = "/share/nas2-amap/gvieilledent/Africa"
+owd = "/share/nas2-amap/gvieilledent/Asia"
 os.chdir(owd)
 
 # Country isocode
 file_ctry_run = pkg_resources.resource_filename("forestatrisk",
                                                 "data/ctry_run.csv")
 data_ctry_run = pd.read_csv(file_ctry_run, sep=";", header=0)
-iso3 = list(data_ctry_run.iso3[data_ctry_run.cont_run == "Africa"])
+iso3 = list(data_ctry_run.iso3[data_ctry_run.cont_run == "Asia"])
 iso3.sort()
 # print(iso3)
-# iso3 = ['AGO', 'BDI', 'BEN', 'CAF', 'CIV', 'CMR', 'COD', 'COG', 'COM',
-#         'ETH', 'GAB', 'GHA', 'GIN', 'GMB', 'GNB', 'GNQ', 'KEN', 'LBR',
-#         'MDG', 'MOZ', 'MUS', 'MWI', 'MYT', 'NGA', 'REU', 'RWA', 'SEN',
-#         'SLE', 'SSD', 'STP', 'TGO', 'TZA', 'UGA', 'ZMB']
+# iso3 = ['BGD', 'BRN', 'BTN', 'FJI', 'IDN', 'IND', 'KHM', 'LAO', 'LKA', 'MMR',
+#         'MYS', 'NCL', 'PHL', 'PNG', 'SGP', 'SLB', 'THA', 'TLS', 'VNM', 'VUT']
 
 
 # Function for multiprocessing
@@ -60,19 +58,18 @@ def run_country(iso3):
     far.make_dir(iso3)
     os.chdir(os.path.join(owd, iso3))
     
-    # # Data
-    # far.data.country(iso3=iso3,
-    #                  proj="EPSG:3395",
-    #                  data_country=True,
-    #                  data_forest=True,
-    #                  keep_data_raw=True,
-    #                  fcc_source="jrc",
-    #                  gdrive_remote_rclone="gdrive_gv",
-    #                  gdrive_folder="GEE-forestatrisk-tropics")
+    # Data
+    far.data.country(iso3=iso3,
+                     proj="EPSG:3395",
+                     data_country=True,
+                     data_forest=True,
+                     keep_data_raw=True,
+                     fcc_source="jrc",
+                     gdrive_remote_rclone="gdrive_gv",
+                     gdrive_folder="GEE-forestatrisk-tropics")
     
     # Model and Forecast
-    if not os.path.isfile("output_jrc/fcc_2050.png"):
-        run_modelling_steps(fcc_source="jrc")
+    run_modelling_steps(fcc_source="jrc")
     
     # Return country iso code
     return(iso3)
