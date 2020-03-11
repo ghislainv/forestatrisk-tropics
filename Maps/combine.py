640d32df6fd2596a979b71ee73878dbfa11905d3
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2020-03-11 18:06:01 +0100

    Update combine.py

--- Maps/combine.py
+++ Maps/combine.py
@@ -9,6 +9,7 @@
 # license         :GPLv3
 # ==============================================================================
 
+# Imports
 import os
 import subprocess
 import forestatrisk as far
@@ -52,12 +53,13 @@ for i in range(ncont):
     cmd = "gdal_translate -co 'COMPRESS=LZW' -co 'PREDICTOR=2' -co 'BIGTIFF=YES' \
     prob.vrt prob.tif"
     subprocess.call(cmd, shell=True)
-    # Build overview
-    os.system("gdaladdo -ro -r nearest prob.tif 16")
+    # Resample at 500m
+    subprocess.call("gdalwarp -tap -r near -tr 500 500 -co 'COMPRESS=LZW' \
+    -co 'PREDICTOR=2' -co 'BIGTIFF=YES' prob.tif prob_500m.tif", shell=True)
     # Plot
-    prob = far.plot.prob("prob.tif", output_file="prob.png",
-                         borders="borders.shp", zoom=None, dpi=300,
-                         lw=0.5, c="grey")
+    prob = far.plot.prob("prob_500m.tif", output_file="prob.png", addo=False,
+                borders="borders.shp", zoom=None, dpi=300,
+                lw=0.5, c="grey")
     plt.close(prob)
 
     # Forest cover in 2050
@@ -67,18 +69,19 @@ for i in range(ncont):
     cmd = "gdal_translate -co 'COMPRESS=LZW' -co 'PREDICTOR=2' -co 'BIGTIFF=YES' \
     fcc_2050.vrt fcc_2050.tif"
     subprocess.call(cmd, shell=True)
-    # Build overview
-    os.system("gdaladdo -ro -r nearest fcc_2050.tif 16")
+    # Resample at 500m
+    subprocess.call("gdalwarp -tap -r near -tr 500 500 -co 'COMPRESS=LZW' \
+    -co 'PREDICTOR=2' -co 'BIGTIFF=YES' fcc_2050.tif fcc_2050_500m.tif", shell=True)
     # Plot
-    fcc = far.plot.fcc("fcc_2050.tif", output_file="fcc_2050.png",
+    fcc = far.plot.fcc("fcc_2050_500m.tif", output_file="fcc_2050.png", addo=False,
                        borders="borders.shp", zoom=None, dpi=300,
                        lw=0.5, c="grey")
     plt.close(fcc)
     # Plot for colorblindless
-    fcc = far.plot.fcc("fcc_2050.tif", output_file="fcc_2050_colblind.png",
+    fcc = far.plot.fcc("fcc_2050_500m.tif", output_file="fcc_2050_colblind.png",
                        borders="borders.shp", zoom=None, dpi=300,
-                       # col_for=(30, 136, 229, 255),  # blue
-                       col_for=(0, 77, 64, 255),  # green
+                       col_for=(30, 136, 229, 255),  # blue
+                       #col_for=(0, 77, 64, 255),  # green
                        col_defor=(255, 193, 7, 255),  # yellow
                        lw=0.5, c="grey")
     plt.close(fcc)
