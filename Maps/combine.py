f177b4f16dd003954f897646a109139e26d3bfec
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2020-03-12 18:18:44 +0100

    Update Maps

--- Maps/combine.py
+++ Maps/combine.py
@@ -16,8 +16,8 @@ import forestatrisk as far
 import matplotlib.pyplot as plt
 
 # Set PROJ_LIB
-PROJ_LIB = "/home/gvieilledent/.pyenv/versions/miniconda3-latest/envs/conda-far/share/proj"
-os.environ["PROJ_LIB"] = PROJ_LIB
+#PROJ_LIB = "/home/gvieilledent/.pyenv/versions/miniconda3-latest/envs/conda-far/share/proj"
+#os.environ["PROJ_LIB"] = PROJ_LIB
 
 # List of continents
 continent = ["Africa", "America", "Asia"]
@@ -40,13 +40,17 @@ for i in range(ncont):
     # Make directory
     far.make_dir(cont)
     os.chdir(os.path.expanduser("~/Code/forestatrisk-tropics/Maps/" + cont))
-    
+
+    # =======================
     # Combine country borders
+    # =======================
     cmd = "find ~/nas/ -regextype posix-egrep -regex '.*" + cont_regex + ".*ctry_PROJ.shp$' \
     -exec ogr2ogr -update -append borders.shp {} \;"
     subprocess.call(cmd, shell=True)
 
+    # ===================
     # Spatial probability
+    # ===================
     cmd = "find ~/nas/ -regextype posix-egrep -regex '.*" + cont_regex + ".*prob.tif$' > list_prob.txt"
     subprocess.call(cmd, shell=True)
     subprocess.call("gdalbuildvrt -input_file_list list_prob.txt prob.vrt", shell=True)
@@ -62,7 +66,9 @@ for i in range(ncont):
                 lw=0.5, c="grey")
     plt.close(prob)
 
+    # ====================
     # Forest cover in 2050
+    # ====================
     cmd = "find ~/nas/ -regextype posix-egrep -regex '.*" + cont_regex + ".*fcc_2050.tif$' > list_fcc_2050.txt"
     subprocess.call(cmd, shell=True)
     subprocess.call("gdalbuildvrt -input_file_list list_fcc_2050.txt fcc_2050.vrt", shell=True)
@@ -85,6 +91,33 @@ for i in range(ncont):
                        col_defor=(255, 193, 7, 255),  # yellow
                        lw=0.5, c="grey")
     plt.close(fcc)
+
+    # =============================
+    # Forest cover change 2019-2050
+    # =============================
+    cmd = "find ~/nas/ -regextype posix-egrep -regex '.*" + cont_regex + ".*fcc23.tif$' > list_fcc23.txt"
+    subprocess.call(cmd, shell=True)
+    subprocess.call("gdalbuildvrt -input_file_list list_fcc_2050.txt fcc_2050.vrt", shell=True)
+    cmd = "gdal_translate -co 'COMPRESS=LZW' -co 'PREDICTOR=2' -co 'BIGTIFF=YES' \
+    fcc_2050.vrt fcc_2050.tif"
+    subprocess.call(cmd, shell=True)
+    # Resample at 500m
+    subprocess.call("gdalwarp -tap -r near -tr 500 500 -co 'COMPRESS=LZW' \
+    -co 'PREDICTOR=2' -co 'BIGTIFF=YES' fcc_2050.tif fcc_2050_500m.tif", shell=True)
+    # Plot
+    fcc = far.plot.fcc("fcc_2050_500m.tif", output_file="fcc_2050.png", addo=False,
+                       borders="borders.shp", zoom=None, dpi=300,
+                       lw=0.5, c="grey")
+    plt.close(fcc)
+    # Plot for colorblindless
+    fcc = far.plot.fcc("fcc_2050_500m.tif", output_file="fcc_2050_colblind.png",
+                       borders="borders.shp", zoom=None, dpi=300,
+                       col_for=(30, 136, 229, 255),  # blue
+                       #col_for=(0, 77, 64, 255),  # green
+                       col_defor=(255, 193, 7, 255),  # yellow
+                       lw=0.5, c="grey")
+    plt.close(fcc)
+    
 
     # Return to owd
     os.chdir(owd)
