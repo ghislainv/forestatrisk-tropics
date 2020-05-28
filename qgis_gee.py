370512c49a4f070c4331e517e4b31ef5b0a5098e
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2020-05-28 17:39:43 +0200

    Add qgis_gee.py

---
+++ qgis_gee.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ==============================================================================
+# author          :Ghislain Vieilledent
+# email           :ghislain.vieilledent@cirad.fr, ghislainv@gmail.com
+# web             :https://ecology.ghislainv.fr
+# python_version  :>=2.7
+# license         :GPLv3
+# ==============================================================================
+
+# Annual product legend
+# 1. Tropical moist forest (TMF including bamboo-dominated forest and mangroves)
+# 2. TMF converted later in a tree plantation
+# 3. NEW degradation 
+# 4. Ongoing degradation (disturbances still detected - can be few years after first degrad if several degradation stages)
+# 5. Degraded forest (former degradation, no disturbances detected anymore)
+# 6. NEW deforestation (may follow degradation)
+# 7. Ongoing deforestation (disturbances still detected)
+# 8. NEW Regrowth
+# 9. Regrowthing
+# 10. Other land cover (not water)
+# 11. Permanent Water (pekel et al.2015)     
+# 12. Seasonal Water (pekel et al.2015) 
+# 13. Init period without valid data - Init class = TMF
+# 14. Init period with min 1 valid obs - Init class = TMF
+# 15. Nodata  - Init class = other LC 
+# 16. Init period without valid data - Init class = Plantation
+
+# Imports
+import ee
+from ee_plugin import Map
+
+# Extent
+iso3 = "REU"
+#iso3 = "MDG"
+if iso3 == "REU":
+    extent_latlong = [55.216251, -21.389860, 55.837360, -20.871805]  # REU
+elif iso3 == "MDG":
+    extent_latlong = [43.10, -25.15, 51.70, -11.80]  # MDG
+else:
+    extent_latlong = [49.307035, -16.028757, 50.526517, -14.810834]  # Masoala
+
+# Region
+region = ee.Geometry.Rectangle(extent_latlong, proj="EPSG:4326",
+                               geodesic=False)
+region = region.buffer(10000).bounds()
+export_coord = region.getInfo()["coordinates"]
+
+# Corrections
+Transition=ee.ImageCollection(
+    "users/ClassifLandsat072015/Roadless2019/TransitionMap_1982_2019").mosaic().unmask()
+OtherLC = (Transition.gte(90)).Or(Transition.eq(71)).Or(Transition.eq(72))
+AnnualChangeYear = ee.ImageCollection(
+    "users/ClassifLandsat072015/Roadless2019/AnnualChanges_1982_2019").mosaic().toByte()
+AnnualChange = AnnualChangeYear.where(
+    OtherLC.And((AnnualChangeYear.eq(13)).
+        Or(AnnualChangeYear.eq(14)).
+            Or(AnnualChangeYear.eq(16))),15)
+AP = AnnualChange.clip(region)
+
+# Path to JRC products
+#path = "users/ghislainv/jrc/"
+
+# JRC annual product (AP)
+#AP = ee.ImageCollection(path + "AnnualChanges1982_2018")
+#AP = ee.ImageCollection(
+#    "users/ClassifLandsat072015/Roadless2019/AnnualChanges_1982_2019")
+#AP = AP.mosaic().toByte().clip(region)
+
+#Map.addLayer(AP.updateMask(AP), {}, "forest2000-2020", True)
+
+# ap_allYear: forest if Y = 1, 2, 3, 4, 5, 13, 14.
+AP_forest = AP.where(AP.eq(2).Or(AP.eq(3)).Or(AP.eq(4)).Or(
+    AP.eq(5)).Or(AP.eq(13)).Or(AP.eq(14)), 1)
+ap_allYear = AP_forest.where(AP_forest.neq(1), 0)
+
+# Forest in Jan 2020
+forest2020 = ap_allYear.select(37)
+
+# Forest cover Jan 2015
+ap_2015_2020 = ap_allYear.select(list(range(32, 38)))
+forest2015 = ap_2015_2020.reduce(ee.Reducer.sum())
+forest2015 = forest2015.gte(1)
+
+# Forest cover Jan 2010
+ap_2010_2020 = ap_allYear.select(list(range(27, 38)))
+forest2010 = ap_2010_2020.reduce(ee.Reducer.sum())
+forest2010 = forest2010.gte(1)
+
+# Forest cover Jan 2005
+ap_2005_2020 = ap_allYear.select(list(range(22, 38)))
+forest2005 = ap_2005_2020.reduce(ee.Reducer.sum())
+forest2005 = forest2005.gte(1)
+
+# Forest cover Jan 2000
+ap_2000_2020 = ap_allYear.select(list(range(17, 38)))
+forest2000 = ap_2000_2020.reduce(ee.Reducer.sum())
+forest2000 = forest2000.gte(1)
+
+# Forest raster with five bands
+forest = forest2000.addBands(forest2005).addBands(
+    forest2010).addBands(forest2015).addBands(forest2020)
+forest = forest.select([0, 1, 2, 3, 4], ["forest2000", "forest2005",
+                                         "forest2010", "forest2015",
+                                         "forest2020"])
+forest = forest.set("system:bandNames", ["forest2000", "forest2005",
+                                         "forest2010", "forest2015",
+                                         "forest2020"])
+
+# rgb2hex
+def rgb2hex(r, g, b):
+    hex = "#{:02x}{:02x}{:02x}".format(r, g, b)
+    return hex
+
+# Palette
+Palette = [rgb2hex(255, 255, 255), rgb2hex(255, 165, 0), rgb2hex(227, 26, 28), rgb2hex(34, 139, 34)]
+
+# Forest raster with three values
+forest = forest2000.where(forest2010.eq(1), 2).where(forest2020.eq(1), 3)
+
+# Map
+Map.addLayer(forest.updateMask(forest), {"palette": Palette}, "forest2000-2020", True)
+if iso3 == "REU":
+    x, y, z = (55.53, -21.11, 12)
+elif iso3 == "MDG":
+    x, y, z = (46.70, -19.30, 8)
+else:
+    x, y, z = (50.30, -15.47, 12)
+Map.setCenter(x, y, z)
+
+# EOF
