e824865b473fbd0d9f41bf20ba811a51361cc0f6
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2020-07-13 11:53:30 +0200

    Update backup

--- Backups/rsync_fdb.sh
+++ Backups/rsync_fdb.sh
@@ -13,15 +13,15 @@ dest_path="/home/forestatrisk-tropics/gfc2020_70/"
 data_raw="*/data_raw/*"
 rsync -e "ssh -i /home/ghislain/.ssh/id_rsa" -av --exclude $data_raw --delete-after --stats --progress $source_path $dest_path
 
-# # jrc2020
-# source_path="mbb:/share/nas2-amap/gvieilledent/jrc2020/"
-# dest_path="/home/forestatrisk-tropics/jrc2020/"
-# data_raw="*/data_raw/*"
-# rsync -e "ssh -i /home/ghislain/.ssh/id_rsa" -av --exclude $data_raw --delete-after --stats --progress $source_path $dest_path
+# jrc2020
+source_path="mbb:/share/nas2-amap/gvieilledent/jrc2020/"
+dest_path="/home/forestatrisk-tropics/jrc2020/"
+data_raw="*/data_raw/*"
+rsync -e "ssh -i /home/ghislain/.ssh/id_rsa" -av --exclude $data_raw --delete-after --stats --progress $source_path $dest_path
 
 # home mbb
 source_path="mbb:/home/gvieilledent/"
 dest_path="/home/forestatrisk-tropics/home_mbb/"
-rsync -e "ssh -i /home/ghislain/.ssh/id_rsa" -av --delete-after --stats --progress $source_path $dest_path
+rsync -e "ssh -i /home/ghislain/.ssh/id_rsa" -av --exclude=".singularity" --exclude="Code/forestatrisk-tropics/forestatrisk-tropics.simg" --delete-after --stats --progress $source_path $dest_path
 
 # EOF
