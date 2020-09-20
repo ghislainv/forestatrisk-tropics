5295df6207889e7bd0104f8a082d2e28e26e9a75
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2020-09-20 20:26:22 +0200

    Update combine

--- Maps/combine.py
+++ Maps/combine.py
@@ -5,133 +5,128 @@
 # author          :Ghislain Vieilledent
 # email           :ghislain.vieilledent@cirad.fr, ghislainv@gmail.com
 # web             :https://ghislainv.github.io
-# python_version  :2.7
+# python_version  :>=3.0
 # license         :GPLv3
 # ==============================================================================
 
 # Imports
 import sys
 import os
-os.chdir("/home/gvieilledent/Code/forestatrisk-tropics/Maps/")
+os.chdir("/home/ghislain/Code/forestatrisk-tropics/Maps/")
 import subprocess
 import forestatrisk as far
 import matplotlib.pyplot as plt
 from tif2cog import tif2cog
 
 # Set PROJ_LIB
-#PROJ_LIB = "/home/gvieilledent/.pyenv/versions/miniconda3-latest/envs/conda-far/share/proj"
-#os.environ["PROJ_LIB"] = PROJ_LIB
+# PROJ_LIB = "/home/ghislain/.pyenv/versions/miniconda3-4.7.12/envs/conda-far/share/proj"
+# os.environ["PROJ_LIB"] = PROJ_LIB
 
 # List of continents
 continent = ["Africa", "America", "Asia"]
-#ncont = len(continent)
+ncont = len(continent)
 
-# Loop on continent
-#for i in range(ncont):
+# Directories
+dataset = "jrc2020"
+#rdir = "/home/forestatrisk-tropics/" + dataset
+rdir = "/share/nas2-amap/gvieilledent/" + dataset
 
 index_cont = int(sys.argv[1])-1
 
-
 # run_combine
-def run_combine(i):
+def run_combine(index_cont):
 
     # Cont variable
-    cont = continent[i]
+    cont = continent[index_cont]
     if cont == "America":
         cont_regex = "(America|Brazil)"
     else:
         cont_regex = cont
     # Make directory
-    far.make_dir("/share/nas2-amap/gvieilledent/Maps/" + cont)
-    os.chdir("/share/nas2-amap/gvieilledent/Maps/" + cont)
+    far.make_dir(rdir + "/Maps/" + cont)
+    os.chdir(rdir + "/Maps/" + cont)
 
-    # # =======================
-    # # Combine country borders
-    # # =======================
-    # # Combine
-    # cmd = "find ~/nas/ -regextype posix-egrep -regex '.*" + cont_regex + ".*/data/ctry_PROJ.shp$' \
-    # -exec ogr2ogr -update -append borders.shp {} \;"
-    # subprocess.call(cmd, shell=True)
-    # # Simplify
-    # cmd = "ogr2ogr -overwrite borders_simp.shp borders.shp -simplify 1000"
-    # subprocess.call(cmd, shell=True)
+    # =======================
+    # Combine country borders
+    # =======================
+    # Combine
+    cmd = "find " + rdir + " -regextype posix-egrep -regex '.*" + cont_regex + ".*/data/ctry_PROJ.shp$' \
+    -exec ogr2ogr -update -append -nlt MULTIPOLYGON borders.gpkg {} \;"
+    subprocess.call(cmd, shell=True)
+    # Simplify
+    cmd = "ogr2ogr -overwrite -nlt MULTIPOLYGON borders_simp.gpkg borders.gpkg -simplify 1000"
+    subprocess.call(cmd, shell=True)
 
-    # # ===================
-    # # Spatial probability
-    # # ===================
-    # # List of tif files
-    # cmd = "find ~/nas/ -regextype posix-egrep -regex '.*" + cont_regex + ".*prob.tif$' > list_prob.txt"
-    # subprocess.call(cmd, shell=True)
-    # # COG
-    # tif2cog(input_file_list="list_prob.txt", output_file="prob.tif", num_threads=8)
-    # # Resample at 500m
-    # subprocess.call("gdalwarp -overwrite -multi -wo 'NUM_THREADS=8' -wm 4096 -tap -r near -tr 500 500 -co 'COMPRESS=DEFLATE' \
-    # -co 'PREDICTOR=2' -co 'BIGTIFF=YES' prob.tif prob_500m.tif", shell=True)
+    # ===================
+    # fcc123
+    # ===================
+    # List of tif files
+    cmd = "find " + rdir + " -regextype posix-egrep -regex '.*" + cont_regex + ".*fcc123.tif$' > list_tif.txt"
+    subprocess.call(cmd, shell=True)
+    # COG
+    tif2cog(input_file_list="list_tif.txt", output_file="fcc123.tif", num_threads=8)
+    # Resample at 500m
+    subprocess.call("gdalwarp -overwrite -multi -wo 'NUM_THREADS=8' -wm 4096 -tap -r near -tr 500 500 -co 'COMPRESS=DEFLATE' \
+    -co 'PREDICTOR=2' -co 'BIGTIFF=YES' fcc123.tif fcc123_500m.tif", shell=True)
+    # Plot
+    fcc123 = far.plot.fcc123("fcc123_500m.tif", output_file="fcc123.png",
+                           maxpixels=1e13,
+                           borders="borders_simp.gpkg",
+                           zoom=None, dpi=300,
+                           lw=0.5, c="grey")
+    plt.close(fcc123)
+
+    # ===================
+    # Spatial probability
+    # ===================
+    # List of tif files
+    cmd = "find " + rdir + " -regextype posix-egrep -regex '.*" + cont_regex + ".*prob.tif$' > list_tif.txt"
+    subprocess.call(cmd, shell=True)
+    # COG
+    tif2cog(input_file_list="list_tif.txt", output_file="prob.tif", num_threads=8)
+    # Resample at 500m
+    subprocess.call("gdalwarp -overwrite -multi -wo 'NUM_THREADS=8' -wm 4096 -tap -r near -tr 500 500 -co 'COMPRESS=DEFLATE' \
+    -co 'PREDICTOR=2' -co 'BIGTIFF=YES' prob.tif prob_500m.tif", shell=True)
     # Plot
     prob = far.plot.prob("prob_500m.tif", output_file="prob.png",
                          maxpixels=1e13,
-                         borders="borders_simp.shp",
+                         borders="borders_simp.gpkg",
                          zoom=None, dpi=300,
                          lw=0.5, c="grey")
     plt.close(prob)
 
-    # # ====================
-    # # Forest cover in 2050
-    # # ====================
-    # # List of tif files
-    # cmd = "find ~/nas/ -regextype posix-egrep -regex '.*" + cont_regex + ".*fcc_2050.tif$' > list_fcc_2050.txt"
-    # subprocess.call(cmd, shell=True)
-    # # COG
-    # tif2cog(input_file_list="list_fcc_2050.txt", output_file="fcc_2050.tif", num_threads=8)
-    # # Resample at 500m
-    # subprocess.call("gdalwarp -overwrite -multi -wo 'NUM_THREADS=8' -wm 4096 -tap -r near -tr 500 500 -co 'COMPRESS=LZW' \
-    # -co 'PREDICTOR=2' -co 'BIGTIFF=YES' fcc_2050.tif fcc_2050_500m.tif", shell=True)
-    # Plot
-    fcc = far.plot.fcc("fcc_2050_500m.tif", output_file="fcc_2050.png",
-                       maxpixels=1e13,
-                       borders="borders_simp.shp",
-                       zoom=None, dpi=300,
-                       lw=0.5, c="grey")
-    plt.close(fcc)
-
     # ====================
-    # Forest cover in 2100
+    # Forest cover in 20XX
     # ====================
-    # List of tif files
-    cmd = "find ~/nas/ -regextype posix-egrep -regex '.*" + cont_regex + ".*fcc_2100.tif$' > list_fcc_2100.txt"
-    subprocess.call(cmd, shell=True)
-    # COG
-    tif2cog(input_file_list="list_fcc_2100.txt", output_file="fcc_2100.tif", num_threads=8)
-    # Resample at 500m
-    subprocess.call("gdalwarp -overwrite -multi -wo 'NUM_THREADS=8' -wm 4096 -tap -r near -tr 500 500 -co 'COMPRESS=LZW' \
-    -co 'PREDICTOR=2' -co 'BIGTIFF=YES' fcc_2100.tif fcc_2100_500m.tif", shell=True)
-    # Plot
-    fcc = far.plot.fcc("fcc_2100_500m.tif", output_file="fcc_2100.png",
-                       maxpixels=1e13,
-                       borders="borders_simp.shp",
-                       zoom=None, dpi=300,
-                       lw=0.5, c="grey")
-    plt.close(fcc)
 
-    # # ==================================
-    # # Forest cover change 2000-2010-2019
-    # # ==================================
-    # cmd = "find ~/nas/ -regextype posix-egrep -regex '.*" + cont_regex + ".*data/fcc23.tif$' > list_fcc23.txt"
-    # subprocess.call(cmd, shell=True)
-    # # COG
-    # tif2cog(input_file_list="list_fcc23.txt", output_file="fcc23.tif", num_threads=8)
-    # # Resample at 500m
-    # subprocess.call("gdalwarp -overwrite -multi -wo 'NUM_THREADS=8' -wm 4096 -tap -r near -tr 500 500 -co 'COMPRESS=LZW' \
-    # -co 'PREDICTOR=2' -co 'BIGTIFF=YES' fcc23.tif fcc23_500m.tif", shell=True)
-    # Plot
-    fcc = far.plot.fcc("fcc23_500m.tif", output_file="fcc23.png",
-                       maxpixels=1e13,
-                       borders="borders_simp.shp",
-                       zoom=None, dpi=300,
-                       lw=0.5, c="grey")
-    plt.close(fcc)
+    # Dates
+    dates_fut = [2030, 2035, 2040, 2050, 2055, 2060, 2070, 2080, 2085, 2090, 2100]
+    #dates_fut = [2050, 2085, 2100]
+    ndates_fut = len(dates_fut)
 
-#
+    # Loop on dates
+    for j in range(ndates_fut):
+        # Date
+        d = str(dates_fut[j])
+        # List of tif files
+        cmd = "find " + rdir + " -regextype posix-egrep -regex '.*" + cont_regex + ".*fcc_" + d + ".tif$' > list_tif.txt"
+        subprocess.call(cmd, shell=True)
+        # COG
+        tif2cog(input_file_list="list_tif.txt", output_file="fcc_" + d + ".tif", num_threads=8)
+        # Resample at 500m
+        subprocess.call("gdalwarp -overwrite -multi -wo 'NUM_THREADS=8' -wm 4096 -tap -r near -tr 500 500 -co 'COMPRESS=LZW' \
+        -co 'PREDICTOR=2' -co 'BIGTIFF=YES' fcc_" + d + ".tif fcc_" + d + "_500m.tif", shell=True)
+        # Plot
+        fcc = far.plot.fcc("fcc_" + d + "_500m.tif", output_file="fcc_" + d + ".png",
+                           maxpixels=1e13,
+                           borders="borders_simp.gpkg",
+                           zoom=None, dpi=300,
+                           lw=0.5, c="grey")
+        plt.close(fcc)
+
+# Run funtion
+# Loop on continent
+#for index_cont in range(ncont):
 run_combine(index_cont)
 
-# End
+# EOF
