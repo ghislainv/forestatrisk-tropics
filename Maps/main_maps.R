635f1f36154deaf942e99973dce7b8f7a1778dd8
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2020-10-15 19:03:08 +0200

    Add figures for article

--- Maps/main_maps.R
+++ Maps/main_maps.R
@@ -79,29 +79,28 @@ for (i in 1:ncont) {
 data("World")
 
 ## Bounding boxes
-## Tropics
-ymin_trop <- -2667917; ymax_trop <- 2667917
+## Tropics in latlong
+ymin_trop <- -23.43661; ymax_trop <- 23.43661
 ## Africa
-xmin_Afr <- -1952868; xmax_Afr <- 5700000
-bbox_Afr <- st_bbox(c(xmin=xmin_Afr, xmax=xmax_Afr,
-                      ymin=ymin_trop, ymax=ymax_trop),
-                    crs=st_crs(3395))
-x_size_Afr <- xmax_Afr-xmin_Afr
+bb <- st_bbox(c(xmin=-19, xmax=52,
+                ymin=ymin_trop, ymax=ymax_trop),
+              crs=4326)
+bbox_Afr <- bb %>% st_as_sfc() %>% st_transform(crs=3395) %>% st_bbox()
 ## America
-xmin_Ame <- -10500000; xmax_Ame <- -3600000
-bbox_Ame <- st_bbox(c(xmin=xmin_Ame, xmax=xmin_Ame + x_size_Afr,
-                      ymin=ymin_trop, ymax=ymax_trop),
-                    crs=st_crs(3395))
+bb <- st_bbox(c(xmin=-94, xmax=-33,
+                ymin=ymin_trop, ymax=ymax_trop),
+              crs=4326)
+bbox_Ame <- bb %>% st_as_sfc() %>% st_transform(crs=3395) %>% st_bbox()
 ## Asia
-xmin_Asi <- 7400000; xmax_Asi <- 19100000
-bbox_Asi <- st_bbox(c(xmin=xmin_Asi, xmax=xmax_Asi,
-                      ymin=ymin_trop, ymax=ymax_trop),
-                    crs=st_crs(3395))
-x_size <- 19100000-7400000
+bb <- st_bbox(c(xmin=68, xmax=170,
+                ymin=ymin_trop, ymax=ymax_trop),
+              crs=4326)
+bbox_Asi <- bb %>% st_as_sfc() %>% st_transform(crs=3395) %>% st_bbox()
 bbox_cont <- list(bbox_Afr, bbox_Ame, bbox_Asi)
 
 ## List to save maps
-l_fcc2050 <- list()
+l_fcc2100 <- list()
+l_prob <- list()
   
 ## Loop on continent
 tmap_opt(npix=1e5)
@@ -138,15 +137,16 @@ for (i in 1:ncont) {
 	borders <- st_read(f) 
 	if (dataset=="jrc2020" & cont=="Africa") {borders <- borders %>% filter(GID_0 != "STP")}
 	
-	## Import fcc2100 raster
+	## Import rasters
 	r_fcc2100 <- read_stars(here("Maps", dataset, cont, "fcc_2100_500m.tif"))
+	r_prob <- read_stars(here("Maps", dataset, cont, "prob_500m.tif"))
 	
-	## Maps
-	m_fcc2050 <- 
+	## Maps fcc2050
+	m_fcc2100 <- 
 		tm_shape(eq_sf, bbox=bbox_cont[[i]]) +
 		  tm_lines(lty=1,lwd=0.5) +
-		tm_shape(trop_sf) +
-		  tm_lines(lty=2, lwd=0.5) +
+		#tm_shape(trop_sf) +
+		#  tm_lines(lty=2, lwd=0.5) +
 		tm_shape(gadm0_cont) +
 		  tm_fill(grey(0.9)) +
 	  tm_shape(r_fcc2100) +
@@ -154,24 +154,92 @@ for (i in 1:ncont) {
 	              palette=c(red, green)) +
 	  tm_shape(borders) +
 		  tm_borders(col=grey(0.5), lwd=0.5) +
-	  tm_layout(outer.margins=c(0,0,0,0))
+	  tm_layout(inner.margins=c(0,0,0,0),
+	            outer.margins=c(0,0,0,0))
+	
+	## Maps prob
+	l.show <- ifelse(cont=="Asia", TRUE, FALSE)
+	m_prob <- 
+		tm_shape(eq_sf, bbox=bbox_cont[[i]]) +
+		  tm_lines(lty=1,lwd=0.5) +
+		#tm_shape(trop_sf) +
+		#  tm_lines(lty=2, lwd=0.5) +
+		tm_shape(gadm0_cont) +
+		  tm_fill(grey(0.9)) +
+	  tm_shape(r_prob) +
+	    tm_raster(style="cont", title="", legend.reverse=TRUE, legend.show=l.show,
+	              palette=c("#228b22", "#ffa500", "#e31a1c", "#000000"),
+	              breaks=c(1, 39322, 54249, 65535), labels=c("0","","","1")) +
+	  tm_shape(borders) +
+		  tm_borders(col=grey(0.5), lwd=0.5) +
+	  tm_layout(inner.margins=c(0,0,0,0),
+	            outer.margins=c(0,0,0,0))
 	
 	## Save in list
-	l_fcc2050[[i]] <- m_fcc2050
+	l_fcc2100[[i]] <- m_fcc2100
+	l_prob[[i]] <- m_prob
 }
 
-l_fcc2050[[1]]
-l_fcc2050[[2]]
-l_fcc2050[[3]]
+## Add year and scale bar to Asia
+## fcc2100
+fcc2100_Asia <- l_fcc2100[[3]] + 
+  tm_scale_bar(breaks=c(0, 1000, 2000), text.size=0.6,
+	             position=c(0.5, 0), just=c("center", "bottom")) +
+  tm_layout(title="2100",
+            title.position=c(0.02,0.08), title.size=1.2)
+## prob
+prob_Asia <- l_prob[[3]] + 
+  tm_scale_bar(breaks=c(0, 1000, 2000), text.size=0.6,
+	             position=c(0.5, 0), just=c("center", "bottom")) +
+  tm_legend(position=c("left","bottom"), just=c("left","bottom")) +
+  tm_layout(legend.text.size=0.8)
+
+## Plot (map) size in m
+height_trop_m <- bbox_Ame$ymax-bbox_Ame$ymin
+width_Ame_m <- bbox_Ame$xmax-bbox_Ame$xmin
+width_Afr_m <- bbox_Afr$xmax-bbox_Afr$xmin
+width_Asi_m <- bbox_Asi$xmax-bbox_Asi$xmin
+
+## Figure width in m (map unit)
+space_npc <- 0.015 # White space between plots (in "npc")
+width_fig_m <- (width_Ame_m+width_Afr_m)/(1-space_npc)
+space_m <- space_npc*width_fig_m
+height_fig_m <- 2*height_trop_m + space_m
+ratio <- height_fig_m/width_fig_m
+
+## Plot (map) width and height in npc
+height_trop_npc <- 0.5*(1-space_npc)
+width_Ame_npc <- width_Ame_m/width_fig_m
+width_Afr_npc <- width_Afr_m/width_fig_m
+width_Asi_npc <- width_Asi_m/width_fig_m
+
+## Viewports in npc
+vp_Ame <- viewport(x=0, y=1, width=width_Ame_npc, height=height_trop_npc, just=c(0,1))
+vp_Afr <- viewport(x=1, y=1, width=width_Afr_npc, height=height_trop_npc, just=c(1,1))
+vp_Asi <- viewport(x=0.5, y=0, width=width_Asi_npc, height=height_trop_npc, just=c(0.5,0))
 
 ## Arrange plots with grid package
-f <- here("Maps", dataset, "fcc2050.png")
-png(filename=f, width=textwidth, height=textwidth, units="cm", res=300)
+## fcc2100
+f <- here("Maps", dataset, "fcc2100.png")
+png(filename=f, width=textwidth, height=textwidth*ratio, units="cm", res=300)
 grid.newpage()
-pushViewport(viewport(layout=grid.layout(4,4)))
-print(l_fcc2050[[2]], vp=viewport(layout.pos.row=1:2, layout.pos.col=1:2))
-print(l_fcc2050[[1]], vp=viewport(layout.pos.row=1:2, layout.pos.col=3:4))
-print(l_fcc2050[[3]], vp=viewport(layout.pos.row=3:4, layout.pos.col=1:4))
+print(l_fcc2100[[2]], vp=vp_Ame)
+print(l_fcc2100[[1]], vp=vp_Afr)
+print(fcc2100_Asia, vp=vp_Asi)
 dev.off()
+## Copy for manuscript
+f_doc <- here("Manuscript", "Article", "figures", "fcc2100.png")
+file.copy(from=f, to=f_doc, overwrite=TRUE)
+## prob
+f <- here("Maps", dataset, "prob.png")
+png(filename=f, width=textwidth, height=textwidth*ratio, units="cm", res=300)
+grid.newpage()
+print(l_prob[[2]], vp=vp_Ame)
+print(l_prob[[1]], vp=vp_Afr)
+print(prob_Asia, vp=vp_Asi)
+dev.off()
+## Copy for manuscript
+f_doc <- here("Manuscript", "Article", "figures", "prob.png")
+file.copy(from=f, to=f_doc, overwrite=TRUE)
 
 # EOF
