1f0b502ac01eda1129c3cff0e6c9529929eb7dce
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2022-02-22 15:56:58 +0100

    Adding forecast_further.py

---
+++ Tropics/forecast_further.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# ==============================================================================
+# author          :Ghislain Vieilledent
+# email           :ghislain.vieilledent@cirad.fr, ghislainv@gmail.com
+# web             :https://ghislainv.github.io
+# python_version  :2.7
+# license         :GPLv3
+# ==============================================================================
+
+# PRIOR TO EXECUTING THE FOLLOWING SCRIPT, YOU MUST HAVE CREDENTIALS FOR
+# 1. Google Earth Engine
+# 2. rclone with Google Drive: https://rclone.org/drive/
+# 3. WDPA: https://www.protectedplanet.net/
+
+import os
+import pkg_resources
+# import re  # regular expressions
+import shutil  # for rmtree
+import sys
+
+from dotenv import load_dotenv
+import ee
+import forestatrisk as far
+import pandas as pd
+from pywdpa import get_token
+
+from run_forecast_further import run_forecast_further
+
+index_ctry = int(sys.argv[1])-1
+
+# ==================
+# Settings
+# Earth engine
+ee.Initialize()
+# WDPA API
+load_dotenv("/home/gvieilledent/Code/forestatrisk-tropics/.env")
+get_token()
+# GDAL
+os.environ["GDAL_CACHEMAX"] = "1024"
+# ==================
+
+# Country isocode
+file_ctry_run = pkg_resources.resource_filename("forestatrisk",
+                                                "data/ctry_run.csv")
+data_ctry_run = pd.read_csv(file_ctry_run, sep=";", header=0)
+iso3 = list(data_ctry_run.iso3)
+nctry = len(iso3)  # 120
+
+
+# Function for multiprocessing
+def run_country(iso3):
+
+    # GDAL temp directory
+    far.make_dir("/share/nas2-amap/gvieilledent/tmp/tmp_" + iso3)
+    os.environ["CPL_TMPDIR"] = "/share/nas2-amap/gvieilledent/tmp/tmp_" + iso3
+
+    # Set original working directory
+    cont = data_ctry_run.cont_run[data_ctry_run["iso3"] == iso3].iloc[0]
+    owd = "/share/nas2-amap/gvieilledent/jrc2020/" + cont
+    os.chdir(owd)
+    far.make_dir(iso3)
+    os.chdir(os.path.join(owd, iso3))
+
+    # Forecast
+    run_forecast_further(iso3, fcc_source="jrc")
+
+    # Remove GDAL temp directory
+    shutil.rmtree("/share/nas2-amap/gvieilledent/tmp/tmp_" + iso3)
+
+    # Return country iso code
+    return iso3
+
+
+# Run country
+# for i in range(nctry):
+#     run_country(iso3[i])
+
+run_country(iso3[index_ctry])
+
+# End
---
+++ Tropics/run_forecast_further.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+
+# ==============================================================================
+# author          :Ghislain Vieilledent
+# email           :ghislain.vieilledent@cirad.fr, ghislainv@gmail.com
+# web             :https://ghislainv.github.io
+# python_version  :2.7
+# license         :GPLv3
+# ==============================================================================
+
+import os
+import re
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import forestatrisk as far
+
+
+# run_forecast_further
+def run_forecast_further(iso3, fcc_source="jrc"):
+    """Forecast the forest cover further in the future.
+
+    Forecast the forest cover further in the future following a
+    business-as-usual scenario and estimate associated carbone
+    emissions.
+
+    :param iso3: Country ISO 3166-1 alpha-3 code. This is used to
+    handle exceptions in the modelling process for specific countries.
+
+    :param fcc_source: Source of forest cover data. Either "jrc" or
+    "gfc".
+
+    """
+
+    # Make output directory
+    far.make_dir("output")
+
+    # ========================================================
+    # Deforestation scenarios
+    # ========================================================
+
+    # Check if Brazil
+    p = re.compile("BRA-.*")
+    m = p.match(iso3)
+
+    # Scenarios
+    scenarios = ["mean", "min", "max"]
+    nscen = len(scenarios)
+
+    # Deforestation estimates with uncertainty
+    f = os.path.expanduser("~/Code/forestatrisk-tropics/"
+                           "Intensity/output/d_uncertainty.csv")
+    d_est = pd.read_csv(f)
+
+    # Loop on scenarios
+    for k in range(nscen):
+
+        # ---------------------------------------------------------
+        # Preparing
+        # --------------------------------------------------------
+
+        scen = scenarios[k]
+
+        # Make directory
+        far.make_dir("output/" + scen)
+
+        # Annual deforested area (ha.yr-1)
+        annual_defor = d_est.loc[d_est["iso3"] == iso3,
+                                 "d_" + scen].values[0]
+
+        # For Brazil
+        if m is not None:
+            if (fcc_source == "jrc"):
+                ifile = "../fcc_BRA_jrc_{}.csv".format(scen)
+                fcc_BRA = pd.read_csv(ifile)
+            else:
+                ifile = "../fcc_BRA_gfc_{}.csv".format(scen)
+                fcc_BRA = pd.read_csv(ifile)
+
+        # Dates and time intervals
+        dates_fut = ["2110", "2120", "2130", "2140", "2150"]
+        ndates_fut = len(dates_fut)
+        ti = [90, 100, 110, 120, 130]
+
+        # --------------------------------------------------------
+        # Predicting forest cover change
+        # --------------------------------------------------------
+
+        # Loop on dates
+        for i in range(ndates_fut):
+            # Amount of deforestation (ha)
+            if m is not None:  # For Brazil
+                defor = fcc_BRA.loc[fcc_BRA["iso3"] == iso3,
+                                    "defor" + dates_fut[i]].values[0]
+            else:
+                defor = np.rint(annual_defor * ti[i])
+            # Compute future forest cover
+            ofile = "output/{}/fcc_{}.tif".format(scen, dates_fut[i])
+            far.deforest(
+                input_raster="output/prob.tif",
+                hectares=defor,
+                output_file=ofile,
+                blk_rows=128)
+
+        # --------------------------------------------------------
+        # Carbon emissions
+        # --------------------------------------------------------
+
+        # Create dataframe
+        C_df = pd.DataFrame(
+            {"date": dates_fut, "C": np.repeat(-99, ndates_fut)},
+            columns=["date", "C"])
+        # Loop on date
+        for i in range(ndates_fut):
+            ifile = "output/{}/fcc_{}.tif".format(scen, dates_fut[i])
+            carbon = far.emissions(
+                input_stocks="data/emissions/AGB.tif",
+                input_forest=ifile)
+            C_df.loc[C_df["date"] == dates_fut[i], ["C"]] = carbon
+        # Save dataframe
+        ofile = "output/{}/C_emissions_further.csv".format(scen)
+        C_df.to_csv(ofile, header=True, index=False)
+
+        # --------------------------------------------------------
+        # Figures
+        # --------------------------------------------------------
+
+        # Projected future forest-cover change
+        for i in range(ndates_fut):
+            ifile = "output/{}/fcc_{}.tif".format(scen, dates_fut[i])
+            ofile = "output/{}/fcc_{}.png".format(scen, dates_fut[i])
+            fig_fcc = far.plot.fcc(
+                ifile,
+                maxpixels=1e8,
+                borders="data/ctry_PROJ.shp",
+                output_file=ofile)
+            plt.close(fig_fcc)
+
+# End
