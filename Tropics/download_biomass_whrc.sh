16e192cb22de22fe071c0d13e4d5fbe2ad722882
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2022-04-06 15:28:50 +0200

    Corrections slurm

--- Tropics/download_biomass_whrc.sh
+++ Tropics/download_biomass_whrc.sh
@@ -2,15 +2,16 @@
 #SBATCH --job-name=getBiomass
 #SBATCH --account=agap
 #SBATCH --partition=agap_normal
-#SBATCH --output=/lustre/vieilledentg/getBiomass_log.txt
-#SBATCH --time=24:00:00
-#SBATCH --ntasks=120
+#SBATCH --output=/lustre/vieilledentg/getBiomass_log.%A_%a.txt
+#SBATCH --error=/lustre/vieilledentg/getBiomass_err.%A_%a.txt
+#SBATCH --time=47:50:00
 #SBATCH --cpus-per-task=3
+#SBATCH --array=1-120
 
 # Loading singularity module
 module load singularity
 
 # run python script inside container
-singularity exec --bind /storage/replicated/cirad/projects/AMAP/vieilledentg ~/replicated/singularity_images/forestatrisk-tropics.simg python3 -u ~/Code/forestatrisk-tropics/Tropics/download_biomass_whrc.py $SLURM_JOB_ID
+singularity exec --bind /storage/replicated/cirad/projects/AMAP/vieilledentg ~/replicated/singularity_images/forestatrisk-tropics.simg python3 -u ~/Code/forestatrisk-tropics/Tropics/download_biomass_whrc.py $SLURM_ARRAY_TASK_ID
 
 # End
