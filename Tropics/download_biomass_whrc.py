2f20e06a900f02d7fcdcd47fd336179ab62f0176
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2022-04-07 15:54:42 +0200

    Correcting projections

--- Tropics/download_biomass_whrc.py
+++ Tropics/download_biomass_whrc.py
@@ -54,65 +54,65 @@ iso3 = list(data_ctry_run.iso3)
 nctry = len(iso3)  # 120
 
 # Projections
-proj_aea_Afr = ("+proj=aea +lat_1=20 +lat_2=-23 +lat_0=0 +lon_0=25 "
-                "+x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")
-proj_aea_Asi = ("+proj=aea +lat_1=7 +lat_2=-32 +lat_0=-15 +lon_0=125 "
-                "+x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")
-proj_aea_Ame = ("+proj=aea +lat_1=-5 +lat_2=-42 +lat_0=-32 +lon_0=-60 "
-                "+x_0=0 +y_0=0 +ellps=aust_SA +units=m no_defs")
-proj_aea_Afr_wkt = ('PROJCS["Africa_Albers_Equal_Area_Conic",'
-                    'GEOGCS["GCS_WGS_1984",'
-                    'DATUM["WGS_1984",'
-                    'SPHEROID["WGS_1984",6378137,298.257223563]],'
-                    'PRIMEM["Greenwich",0],'
-                    'UNIT["Degree",0.017453292519943295]],'
-                    'PROJECTION["Albers_Conic_Equal_Area"],'
-                    'PARAMETER["False_Easting",0],'
-                    'PARAMETER["False_Northing",0],'
-                    'PARAMETER["longitude_of_center",25],'
-                    'PARAMETER["Standard_Parallel_1",20],'
-                    'PARAMETER["Standard_Parallel_2",-23],'
-                    'PARAMETER["latitude_of_center",0],'
-                    'UNIT["Meter",1],'
-                    'AUTHORITY["EPSG","102022"]]')
-proj_aea_Asi_wkt = ('PROJCS["Asia_South_Albers_Equal_Area_Conic",'
-                    'GEOGCS["GCS_WGS_1984",'
-                    'DATUM["WGS_1984",'
-                    'SPHEROID["WGS_1984",6378137,298.257223563]],'
-                    'PRIMEM["Greenwich",0],'
-                    'UNIT["Degree",0.017453292519943295]],'
-                    'PROJECTION["Albers_Conic_Equal_Area"],'
-                    'PARAMETER["False_Easting",0],'
-                    'PARAMETER["False_Northing",0],'
-                    'PARAMETER["longitude_of_center",125],'
-                    'PARAMETER["Standard_Parallel_1",7],'
-                    'PARAMETER["Standard_Parallel_2",-32],'
-                    'PARAMETER["latitude_of_center",-15],'
-                    'UNIT["Meter",1],'
-                    'AUTHORITY["EPSG","102028"]]')
-proj_aea_Ame_wkt = ('PROJCS["South_America_Albers_Equal_Area_Conic",'
-                    'GEOGCS["GCS_South_American_1969",'
-                    'DATUM["South_American_Datum_1969",'
-                    'SPHEROID["GRS_1967_Truncated",6378160,298.25]],'
-                    'PRIMEM["Greenwich",0],'
-                    'UNIT["Degree",0.017453292519943295]],'
-                    'PROJECTION["Albers_Conic_Equal_Area"],'
-                    'PARAMETER["False_Easting",0],'
-                    'PARAMETER["False_Northing",0],'
-                    'PARAMETER["longitude_of_center",-60],'
-                    'PARAMETER["Standard_Parallel_1",-5],'
-                    'PARAMETER["Standard_Parallel_2",-42],'
-                    'PARAMETER["latitude_of_center",-32],'
-                    'UNIT["Meter",1],'
-                    'AUTHORITY["EPSG","102033"]]')
+proj_Afr_proj4 = ("+proj=aea +lat_1=20 +lat_2=-23 +lat_0=0 +lon_0=25 "
+                  "+x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")
+proj_Asi_proj4 = ("+proj=aea +lat_1=7 +lat_2=-32 +lat_0=-15 +lon_0=125 "
+                  "+x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")
+proj_Ame_proj4 = ("+proj=aea +lat_1=-5 +lat_2=-42 +lat_0=-32 +lon_0=-60 "
+                  "+x_0=0 +y_0=0 +ellps=aust_SA +units=m no_defs")
+proj_Afr_wkt = ('PROJCS["Africa_Albers_Equal_Area_Conic",'
+                'GEOGCS["GCS_WGS_1984",'
+                'DATUM["WGS_1984",'
+                'SPHEROID["WGS_1984",6378137,298.257223563]],'
+                'PRIMEM["Greenwich",0],'
+                'UNIT["Degree",0.017453292519943295]],'
+                'PROJECTION["Albers_Conic_Equal_Area"],'
+                'PARAMETER["False_Easting",0],'
+                'PARAMETER["False_Northing",0],'
+                'PARAMETER["longitude_of_center",25],'
+                'PARAMETER["Standard_Parallel_1",20],'
+                'PARAMETER["Standard_Parallel_2",-23],'
+                'PARAMETER["latitude_of_center",0],'
+                'UNIT["Meter",1],'
+                'AUTHORITY["EPSG","102022"]]')
+proj_Asi_wkt = ('PROJCS["Asia_South_Albers_Equal_Area_Conic",'
+                'GEOGCS["GCS_WGS_1984",'
+                'DATUM["WGS_1984",'
+                'SPHEROID["WGS_1984",6378137,298.257223563]],'
+                'PRIMEM["Greenwich",0],'
+                'UNIT["Degree",0.017453292519943295]],'
+                'PROJECTION["Albers_Conic_Equal_Area"],'
+                'PARAMETER["False_Easting",0],'
+                'PARAMETER["False_Northing",0],'
+                'PARAMETER["longitude_of_center",125],'
+                'PARAMETER["Standard_Parallel_1",7],'
+                'PARAMETER["Standard_Parallel_2",-32],'
+                'PARAMETER["latitude_of_center",-15],'
+                'UNIT["Meter",1],'
+                'AUTHORITY["EPSG","102028"]]')
+proj_Ame_wkt = ('PROJCS["South_America_Albers_Equal_Area_Conic",'
+                'GEOGCS["GCS_South_American_1969",'
+                'DATUM["South_American_Datum_1969",'
+                'SPHEROID["GRS_1967_Truncated",6378160,298.25]],'
+                'PRIMEM["Greenwich",0],'
+                'UNIT["Degree",0.017453292519943295]],'
+                'PROJECTION["Albers_Conic_Equal_Area"],'
+                'PARAMETER["False_Easting",0],'
+                'PARAMETER["False_Northing",0],'
+                'PARAMETER["longitude_of_center",-60],'
+                'PARAMETER["Standard_Parallel_1",-5],'
+                'PARAMETER["Standard_Parallel_2",-42],'
+                'PARAMETER["latitude_of_center",-32],'
+                'UNIT["Meter",1],'
+                'AUTHORITY["EPSG","102033"]]')
 
 
 # Function for multiprocessing
 def run_country(iso3):
 
-    # GDAL temp directory
-    far.make_dir(temp_dir + "tmp_" + iso3)
-    os.environ["CPL_TMPDIR"] = temp_dir + "tmp_" + iso3
+    # # GDAL temp directory
+    # far.make_dir(temp_dir + "tmp_" + iso3)
+    # os.environ["CPL_TMPDIR"] = temp_dir + "tmp_" + iso3
 
     # Set original working directory
     cont = data_ctry_run.cont_run[data_ctry_run["iso3"] == iso3].iloc[0]
@@ -123,49 +123,49 @@ def run_country(iso3):
 
     # Albers Equal Area projections
     if cont == "Africa":
-        proj_aea = proj_aea_Afr
-        proj_wkt = proj_aea_Afr_wkt
+        proj_proj4 = proj_Afr_proj4
+        proj_wkt = proj_Afr_wkt
     elif cont == "Asia":
-        proj_aea = proj_aea_Asi
-        proj_wkt = proj_aea_Afr_wkt
+        proj_proj4 = proj_Asi_proj4
+        proj_wkt = proj_Asi_wkt
     else:
-        proj_aea = proj_aea_Ame
-        proj_wkt = proj_aea_Afr_wkt
+        proj_proj4 = proj_Ame_proj4
+        proj_wkt = proj_Ame_wkt
 
-    # # Extract data for country on GEE
-    # far.data.country_biomass_run(
-    #     iso3=iso3,
-    #     proj=ee.Projection(proj_wkt),
-    #     output_dir="data_raw",
-    #     keep_dir=True,
-    #     gdrive_remote_rclone="gdrive_gv",
-    #     gdrive_folder="GEE_biomass_whrc")
-
-    # Download data locally from Google Drive
-    far.data.country_biomass_download(
+    # Extract data for country on GEE
+    far.data.country_biomass_run(
         iso3=iso3,
+        proj=ee.Projection(proj_wkt),
+        output_dir="data_raw",
+        keep_dir=True,
         gdrive_remote_rclone="gdrive_gv",
-        gdrive_folder="GEE_biomass_whrc",
-        output_dir="data_raw")
+        gdrive_folder="GEE_biomass_whrc")
 
-    # Mosaic
-    far.data.country_biomass_mosaic(
-        iso3=iso3,
-        input_dir="data_raw",
-        output_dir="data/emissions",
-        proj=proj_aea)
+    # # Download data locally from Google Drive
+    # far.data.country_biomass_download(
+    #     iso3=iso3,
+    #     gdrive_remote_rclone="gdrive_gv",
+    #     gdrive_folder="GEE_biomass_whrc",
+    #     output_dir="data_raw")
 
-    # Remove GDAL temp directory
-    shutil.rmtree(temp_dir + "tmp_" + iso3)
+    # # Mosaic
+    # far.data.country_biomass_mosaic(
+    #     iso3=iso3,
+    #     input_dir="data_raw",
+    #     output_dir="data/emissions",
+    #     proj=proj_proj4)
+
+    # # Remove GDAL temp directory
+    # shutil.rmtree(temp_dir + "tmp_" + iso3)
 
     # Print country iso code
     print(iso3)
 
 
-# # Run country
-# for i in range(nctry):
-#     run_country(iso3[i])
+# Run country
+for i in range(nctry):
+    run_country(iso3[i])
 
-run_country(iso3[index_ctry])
+# run_country(iso3[index_ctry])
 
 # End
